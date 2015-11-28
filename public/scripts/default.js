28a33f86edd26a7d7417352bba3408234ccabff6
Aneudy Guzman <aneguzman@gmail.com>
2015-11-28 17:19:21 -0400

    Refactoring js code to use promises

--- public/scripts/default.js
+++ public/scripts/default.js
@@ -1,14 +1,63 @@
 $(document).ready(function () {
-	$('#btnAdd').on('click', addNumbers)
+	$('#btnAdd').on('click', addNumbers);
+	$('#btnSubtract').on('click', subtractNumbers);
+	$('#btnMultiplication').on('click', multiplyNumbers);
+	$('#btnDivision').on('click', divideNumbers);
 });
  
 function addNumbers() {
-	var x = document.getElementById('x').value;
-	var y = document.getElementById('y').value;
-	var result = document.getElementById('result');
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET", "/addition?x=" + x + "&y=" + y , false);
-	xmlhttp.send();
-	var jsonResponse = JSON.parse(xmlhttp.response);
-	result.innerHTML = jsonResponse.result;
+	var data = getData();
+	serverAddition(data).done(displayResults);
+}
+
+function subtractNumbers(){
+	var data = getData();
+	serverSubtraction(data).done(displayResults);
+}
+
+function multiplyNumbers(){
+	var data = getData();
+	serverMultiplication(data).done(displayResults);
+}
+function divideNumbers(){
+	var data = getData();
+	serverDivision(data).done(displayResults);
+}
+
+function getData(){
+	var x = $('#x').val();
+	var y = $('#y').val();
+	return {'x' : x,	'y': y	};
+}
+
+function displayResults(serverData){
+	$('#result').html(serverData.result);
+}
+
+function serverAddition(data){
+	return $.getJSON('/addition', data);
+}
+
+function serverSubtraction(data){
+	return $.post('/subtraction', data, 'json');
+}
+
+function serverMultiplication(data){
+	return $.ajax({
+		url: '/multiply',
+		data: data,
+		type: "PUT",
+		dataType: "json",
+		cache: false
+	});
+}
+
+function serverDivision(data){
+	return $.ajax({
+		url: '/divide',
+		data: data,
+		type: "DELETE",
+		dataType: "json",
+		cache: false
+	});
 }
